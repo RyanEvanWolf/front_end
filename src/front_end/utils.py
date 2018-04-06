a426b50022e7e8ceef4a0b09f7a3e47408d72d04
Ryan Evan Wolf <ryanevwolf@gmail.com>
2018-04-06 15:24:32 +0200

    Adjusted build structure to allow for python modules to be exported. This should help make the interfaces a lot cleaner and reduce multiple copies of the same code

---
+++ src/front_end/utils.py
@@ -0,0 +1,14 @@
+
+from front_end.msg import DetectorParameter
+
+def getFastAttributes():
+    return 1
+
+
+
+
+
+def say(name):
+    a=DetectorParameter()
+    print(a)
+    print('Hello ttiahtt +  ' + name)
